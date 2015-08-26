b40e37cf5aed2e3c1eee5a21c599e7432657444b
Wojciech Król <wjch.krl@gmail.com>
2015-08-26 15:03:48 +0200

    Converted to generic

--- Afrodite/Connection/ClientConnectionProvider.cs
+++ Afrodite/Connection/ClientConnectionProvider.cs
@@ -81,6 +81,7 @@ namespace Afrodite.Connection
                         throw new InvalidOperationException("Master didn't ACK");
                     }
                 }
+
             }
         }
 
